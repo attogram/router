a6ba507e9ffae9dd7584f85b6cfae4515e0e687e
Attogram Project <attogram@users.noreply.github.com>
2019-03-28 19:51:57 +0100

    test setup

--- public/index.php
+++ public/index.php
@@ -119,33 +119,34 @@ base      : <?= $base; ?> &nbsp;
 relative  : <?= $relative; ?> &nbsp;
 forceSlash: <?= $forceSlash ? 'true' : 'false' ?> &nbsp; [<a href="?forceSlash=1">Force Slash</a> - <a href="?forceSlash=0">Do Not Force Slash</a>] &nbsp;
 vars      : <?php
-    if (!empty($vars)) {
-        foreach ($vars as $name => $value) {
-            print "$name=$value &nbsp; ";
-        }
-    } else {
-        print '<span class="box empty">null</span>';
-    } ?> &nbsp;
-_GET      : <?php
-    if (!empty($_GET)) {
-        foreach ($_GET as $name => $value) {
-            print htmlentities($name) . '=' . htmlentities($value) . ' &nbsp; ';
-        }
-    } else {
-        print '<span class="box empty">null</span>';
+if (!empty($vars)) {
+    foreach ($vars as $name => $value) {
+        print "$name=$value &nbsp; ";
     }
- ?>
+} else {
+    print '<span class="box empty">null</span>';
+} ?> &nbsp;
+_GET      : <?php
+if (!empty($_GET)) {
+    foreach ($_GET as $name => $value) {
+        print htmlentities($name) . '=' . htmlentities($value) . ' &nbsp; ';
+    }
+} else {
+    print '<span class="box empty">null</span>';
+}
+?>
 </p>
 <hr />
 <ol>
-    <li>[base] <a href="<?= $base; ?>"><?= $base; ?></a></li>
-    <li>[full] <a href="<?= $full; ?>"><?= $full; ?></a></li>
+    <li>{base} <a href="<?= $base; ?>"><?= $base; ?></a></li>
+    <li>{full} <a href="<?= $full; ?>"><?= $full; ?></a></li>
 <?php
-    foreach ($tests as $test) {
-        foreach ($test['test'] as $link) {
-            print '<li>[' . $test['control'] . '] <a href="' . $base . $link . '">' . $base . $link . '</a></li>';
-        }
+foreach ($tests as $test) {
+    foreach ($test['test'] as $link) {
+        print '<li>[' . $test['control'] . '] [' . $test['route'] . ']'
+            . ' <a href="' . $base . $link . '">' . $base . $link . '</a></li>';
     }
+}
 ?>
 </ol>
 <hr />
@@ -153,5 +154,6 @@ _GET      : <?php
  - <a target="_blank" href="https://github.com/attogram/router">github</a>
  - <a target="_blank" href="https://packagist.org/packages/attogram/router">packagist</a>
  - <a target="_blank" href="https://codeclimate.com/github/attogram/router">codeclimate</a>
+ - <a target="_blank" href="https://travis-ci.org/attogram/router">travis-ci</a>
  - <?= date('r'); ?>
 </body></html>
